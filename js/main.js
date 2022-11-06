d82137c8ecf1c41781a44f1284d0b86727d495ce
JasurKhayrullayev <jasurkhayrullayev2002@gmail.com>
2022-11-06 17:56:09 +0500

    delete modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let elProducts = document.querySelector(".products");
+let elModalDesc = document.querySelector(".modal-p");
+let modalBtns = document.querySelector(".modal-footer")
 let productsArray = [];
 (async () => {
     try { 
@@ -23,14 +25,30 @@ function productRender(data) {
                 <p class="product-desc">Count: ${e.rating.count}</p>
                 <p class="product-desc">Desc: ${e.description}</p>
                 <p class="product-desc">Name: ${e.title}</p>
-                <button id=${e.id} class="product-btn">🗑️</button>
+                <button id=${e.id} type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="product-btn">🗑️</button>
     `
     elProducts.appendChild(div)
     });
 }
 
+let deleteProductId
+
 elProducts.addEventListener('click', el => {
-    productsArray = productsArray.filter(e => e.id != el.target.id)
-    elProducts.innerHTML = null;
-    productRender(productsArray)
+    let id = el.target?.id
+    deleteProductId = id;
+    let prod = productsArray.find(pr => pr.id == id)
+    elModalDesc.textContent = `Do you want to delete " ${prod?.title} " ?`
+
+})
+
+modalBtns.addEventListener("click", e=> {
+    if(e.target.id == "delete-btn"){
+        productsArray = productsArray.filter(e => e.id != deleteProductId)
+        elProducts.innerHTML = null;
+        productRender(productsArray)
+    }
+    if(e.target.id == "close-btn"){
+       deleteProductId = null
+    }
+    
 })
